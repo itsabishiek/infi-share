3e7768b695654e43b26bb40006cf1524cbdebd6d
User <s.abishiek007@gmail.com>
2021-09-24 11:33:58 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const connectDB = require("./config/db");
+const File = require("./models/file");
+const fs = require("fs");
+
+connectDB();
+
+// Get all records older than 24 hours
+async function deleteData() {
+  const files = await File.find({
+    createdAt: { $lt: new Date(Date.now() - 24 * 60 * 60 * 1000) },
+  });
+
+  if (files.length) {
+    for (const file of files) {
+      try {
+        fs.unlinkSync(file.path);
+        await file.remove();
+        console.log(`Successfully deleted ${file.filename}`);
+      } catch (err) {
+        console.log(`Error while deleting file ${err}`);
+      }
+    }
+  }
+  console.log("Job done!");
+}
+
+deleteData().then(process.exit);
